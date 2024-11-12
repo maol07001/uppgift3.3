497a881668b5390b605e97a6764fc046441e261a
mariu <mariu@DESKTOP-CT70FK7>
2024-11-12 18:42:56 +0100

    Add project files.

---
+++ uppgift3.3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace uppgift_3._3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int timAvgift = 80;
+            int maxDagAvgift = 950;
+
+            Console.Write("Hur många timmar vill du hyra bilen? ");
+            int timmar = int.Parse(Console.ReadLine());
+
+            int kostnad = timmar * timAvgift;
+
+                if (kostnad > maxDagAvgift)
+                {
+                    kostnad = maxDagAvgift;
+                }
+
+            Console.WriteLine("Kostnaden för att hyra bilen är " + kostnad + " kr.");
+        }
+    }
+}
